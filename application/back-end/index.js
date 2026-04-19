9c7561362b7a5a414733d3d500fa90bc5fac9333
Aleksandra Zec <sanjazec04@gmail.com>
2026-04-19 13:57:11 +0200

    Application setup

---
+++ application/back-end/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+app.get("/",(req,res)=>{
+res.send("hola")
+})
+
+app.listen(process.env.PORT || port, ()=>{
+console.log(`Server is running on port: ${process.env.PORT || port}`)
+})
+
