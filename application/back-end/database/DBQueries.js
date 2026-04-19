748f081bf4a38e8e9cd9cc478ba29dd628549e9f
Nikola Severov <severovnikola8@gmail.com>
2026-04-19 19:37:33 +0200

    fixed some organization

---
+++ application/back-end/database/DBQueries.js
@@ -0,0 +1,51 @@
+import supabase from "../config/supabaseClient.js";
+
+
+export const getUsers = async () => {
+    const { data, error } = await supabase.from("User").select("*");
+    console.log(data);
+    if (error) throw new Error(error.message);
+    return data;
+};
+
+export const getUserById = async (user_id) => {
+    const { data, error } = await supabase
+      .from("User")
+      .select("*")
+      .eq("user_id", user_id)
+      .single();
+    console.log(data);
+    if (error) throw new Error(error.message);
+    return data;
+};
+
+export const createUser = async (user_data) => {
+    console.log([user_data]);
+    const { data, error } = await supabase
+        .from("User")
+        .insert([user_data])
+        .select();
+    if (error) throw new Error(error.message);
+    return data;
+};
+
+export const updateUser = async (user_id, updates) => {
+    const { data, error } = await supabase
+    .from("User")
+    .update(updates)
+    .eq("user_id", user_id)
+    .select();
+
+    if (error) throw new Error(error.message);
+    return data;
+};
+
+export const deleteUser = async (user_id) => {
+const { error } = await supabase
+    .from("User")
+    .delete()
+    .eq("user_id", user_id);
+
+  if (error) throw new Error(error.message);
+  return { success: true, message: "User deleted successfully" };
+};
--- application/back-end/index.js
+++ application/back-end/index.js
@@ -1,92 +1,35 @@
 import express from "express";
+import cors from "cors";
 import env from "dotenv";
-import { createClient } from '@supabase/supabase-js'
 
 env.config();
-
 const app = express();
 const port = 5001;
-const supabase = createClient(process.env.SUPABASE_URL,process.env.SUPABASE_ANON_KEY);
 
+import DB from "./database/DBQueries.js";
 
-app.get("/",(req,res)=>{
-    res.send("SUPABAAAAAAAAAASSEEEE!!!!")
-})
+app.use(express.json());
+
+app.use(express.urlencoded({extended : true}));
+
+app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+   secret: 'some secret',
+   resave: true,
+   saveUninitialized: true,
+   cookie: { secure: false }
+  }))
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD'],
+    credentials: true
+ }))
+
+app.get("/", (req, res) => {
+    res.send("API is Running....")
+});
 
 app.listen(process.env.PORT || port, ()=>{
     console.log(`Server is running on port: ${process.env.PORT || port}`)
 })
-
-// get all users
-app.get("/users", async (_, res) => {
-    try {
-        const { data, error } = await supabase.from("User").select();
-        console.log(data);
-        return res.send(data);
-    } catch (error) {
-        return res.send({ error });
-    }
-});
-
-// get user through id
-app.get("/users/:user_id", async (req, res) => {
-    try {
-        const { data, error } = await supabase
-            .from("User")
-            .select()
-            .eq("user_id", request.params.user_id)
-        console.log(data);
-        return response.send(data);
-    } catch (error) {
-        return response.send({ error })
-    }
-});
-
-// post a user
-app.post("/users", async(req, res) => {
-    try {
-        console.log(req.body);
-        const { data, error } = await supabase.from("User").insert(req.body);
-        if (error) {
-            return res.status(400).json(error);
-        }
-        res.status(200).json(req.body);
-    } catch (error) {
-        res.send({ error });
-    }
-});
-
-// updating a user through id
-app.put("/users/:user_id", async (req, res) => {
-    console.log(req.params);
-    try {
-        const { data: updatedData, error: updatedError } = await supabase
-            .from("User")
-            .update({
-                title: req.body.title ? req.body.title : data[0].title,
-                body: req.body.body ? req.body.body : data[0].body,
-            })
-            .eq("user_id",req.params.user_id);
-            const { data, err } = await supabase.from("User").select();
-            return res.status(200).send(data);
-    } catch (error) {
-        res.send({ error });
-    }
-});
-
-// delete a user
-app.delete("/users/:user_id", async (req, res) => {
-    try {
-        const { data, error } = await supabase
-            .from("User")
-            .delete()
-            .eq("id", req.params.user_id);
-        const { datar, errorr } = await supabase.from("User").select();
-        if (error) {
-            return res.status(400).json(error);
-        }
-        return res.send(datar);
-    } catch (error) {
-        res.send({ error });
-    }
-});
