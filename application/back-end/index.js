4da4665c5d9bf311d6deed9bed6f8bfe9dbecda8
Nikola Severov <severovnikola8@gmail.com>
2026-04-19 18:07:36 +0200

    Backend setup

--- application/back-end/index.js
+++ application/back-end/index.js
@@ -1,12 +1,28 @@
-const express = require('express')
-const app = express()
-const port = 5000
+import express from "express";
+import env from "dotenv";
+import { createClient } from '@supabase/supabase-js'
+
+env.config();
+
+const app = express();
+const port = 5001;
+const supabase = createClient(process.env.SUPABASE_URL,process.env.SUPABASE_ANON_KEY);
+
 
 app.get("/",(req,res)=>{
-res.send("hola")
+    res.send("hola")
 })
 
 app.listen(process.env.PORT || port, ()=>{
-console.log(`Server is running on port: ${process.env.PORT || port}`)
+    console.log(`Server is running on port: ${process.env.PORT || port}`)
 })
 
+app.get("/users", async (_, response) => {
+    try {
+        const { data, error } = await supabase.from("Users").select();
+        console.log(data);
+        return response.send(data);
+    } catch (error) {
+        return response.send({ error });
+    }
+});
