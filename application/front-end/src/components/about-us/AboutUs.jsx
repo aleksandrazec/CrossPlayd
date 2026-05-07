f21db4b817b1d650ed51609621e7409f5d6d6baf
Aleksandra Zec <sanjazec04@gmail.com>
2026-05-07 23:05:04 +0200

    fixed css and html bugs

--- application/front-end/src/components/about-us/AboutUs.jsx
+++ application/front-end/src/components/about-us/AboutUs.jsx
@@ -9,21 +9,23 @@ function AboutUs(props) {
                     <img className="image" src={logo} alt='Logo'></img>
                 </div>
                 <div className="header">
-                    <h1>We're not gamers because we don't have lives. It's because we choose to have many...</h1>
+                    <h1 style={{ textAlign: "center" }}>We're not gamers because we don't have lives. It's because we choose to have many...</h1>
                 </div>
                 <div className="trending-games">
                     <h3>Our mission</h3>
                     <hr></hr>
-                    <p>The purpose of the application CrossPlayd is to allow users to track and review video games they've played all in one place. Rather than being a replacement for other video game scoring sites, this application focuses on creating a user-based community for every game of interest, no matter if the user owns it and independently of the platform they've played it on. This also allows for logging of older or discontinued games, as well as games that are not available on major platforms.</p>
+                    <p style={{ textJustify: "true" }}>The purpose of the application CrossPlayd is to allow users to track and review video games they've played all in one place. Rather than being a replacement for other video game scoring sites, this application focuses on creating a user-based community for every game of interest, no matter if the user owns it and independently of the platform they've played it on. This also allows for logging of older or discontinued games, as well as games that are not available on major platforms.</p>
                     <h3>Creators</h3>
                     <hr></hr>
                     <table className='links'>
-                        <tr>
-                            <th><a href="https://github.com/aleksandrazec">Aleksandra Zec</a></th>
-                            <th><a href="https://github.com/Matej3334">Matej Kunovski</a></th>
-                            <th><a href="https://github.com/CDNSeverov">Nikola Severov</a></th>
-                            <th><a href="https://github.com/AndrejKaz">Andrej Kaziovski</a></th>
-                        </tr>
+                        <tbody>
+                            <tr>
+                                <td><a href="https://github.com/aleksandrazec">Aleksandra Zec</a></td>
+                                <td><a href="https://github.com/Matej3334">Matej Kunovski</a></td>
+                                <td><a href="https://github.com/CDNSeverov">Nikola Severov</a></td>
+                                <td><a href="https://github.com/AndrejKaz">Andrej Kaziovski</a></td>
+                            </tr>
+                        </tbody>
                     </table>
                     <h3>Important links:</h3>
                     <hr></hr>
